1657fee86c344f54af12a0fbef02e5253fdeab73
semisuzume <128959237+semisuzume@users.noreply.github.com>
2023-06-11 16:41:05 +0900

    Update BoardManagement.cs
    不要なDebugを削除した

--- Assets/Script/BoardManagement.cs
+++ Assets/Script/BoardManagement.cs
@@ -178,7 +178,6 @@ public class BoardManagement : MonoBehaviour
             {
                 printString += board[Y, X] + ",";
             }
-            Debug.Log(printString);
         }
     }
 
@@ -274,19 +273,14 @@ public class BoardManagement : MonoBehaviour
         switch (board[frm.y, frm.x].Substring(0, 1))
         {
             case "K":
-                Debug.Log(CheckKing(frm, to));
                 return CheckKing(frm, to);
             case "Q":
-                Debug.Log(CheckQueen(frm, to));
                 return CheckQueen(frm, to);
             case "L":
-                Debug.Log(CheckLuke(frm, to));
                 return CheckLuke(frm, to);
             case "B":
-                Debug.Log(CheckBishop(frm, to));
                 return CheckBishop(frm, to);
             case "N":
-                Debug.Log(CheckKnight(frm, to));
                 return CheckKnight(frm, to);
             case "P":
                 return CheckPawn(player, frm, to);
@@ -323,7 +317,6 @@ public class BoardManagement : MonoBehaviour
             //Debug.Log(movePos.x + "|" + movePos.y);
             if (!(0 <= movePos.x && movePos.x < 8 && 0 <= movePos.y && movePos.y < 8))
             {
-                Debug.Log("配列外です");
                 return false;
             }
 
@@ -334,7 +327,6 @@ public class BoardManagement : MonoBehaviour
             }
             else if (rangeRank != "SS")
             {
-                Debug.Log("妨害されました");
                 return false;
             }
         }
@@ -349,10 +341,8 @@ public class BoardManagement : MonoBehaviour
         {
             //direct 0~7
             int radian = direct * 45;
-            //Debug.Log(radian);
             int sign_x = (int)Math.Round(Math.Cos(radian * (Math.PI / 180)));
             int sign_y = (int)Math.Round(Math.Sin(radian * (Math.PI / 180)));
-            Debug.Log(sign_x.ToString() + ", " + sign_y.ToString());
             flg |= Direct(sign_x, sign_y, frm, to);
         }
 
@@ -451,7 +441,6 @@ public class BoardManagement : MonoBehaviour
         {
             if (ChoicedCheck(player, 1, to))
             {
-                Debug.Log("true");
                 board[frm.y, frm.x] = board[frm.y, frm.x].Substring(0, 3);
                 return true;
             }
@@ -467,14 +456,12 @@ public class BoardManagement : MonoBehaviour
                 {
                     if (ChoicedCheck(player, 2, to))
                     {
-                        Debug.Log("true");
                         CheckPromotion(player, to);
                         return true;
                     }
                 }
             }
         }
-        Debug.Log("false");
         return false;
     }
     //CheckPawn関数から呼び出す
@@ -486,7 +473,6 @@ public class BoardManagement : MonoBehaviour
             image.SetActive(true);
             topFloorTo = to;
             gameManagement.F = true;
-            Debug.Log("Promotion");
         }
     }
     public void Promotion(string rank)
@@ -503,7 +489,6 @@ public class BoardManagement : MonoBehaviour
             color = "B";
         }
         board[topFloorTo.y, topFloorTo.x] = rank + "0" + color;
-        Debug.Log(board[topFloorTo.y, topFloorTo.x]);
         GeneratePiece();
         gameManagement.F = false;
     }
